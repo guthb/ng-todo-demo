957feee6b5043fb83fb1c06d6ede6e13f55dabcb
Bradley Guthrie <guppy96@gmail.com>
2016-05-21 10:41:37 -0500

    task list added

--- app/app.js
+++ app/app.js
@@ -8,5 +8,56 @@ app.controller("NavCtrl", function($scope){
 app.controller("TodoCtrl", function($scope){
 
   $scope.welcome = "hello";
+  $scope.showListView = false;
+  $scope.newTask = {};
+
+  $scope.items =[
+    {
+      id: 0,
+      task: "mow the lawn",
+      isCompleted:true,
+      dueDate:"12/5/17",
+      assignedTo:"greg",
+      location:"Zoe's house",
+      urgency:"low",
+      dependencies:["sunshine", "clippers", "hat", "water", "headphones"]
+
+    },
+    {
+      id: 1,
+      task: "grade quizes",
+      isCompleted:true,
+      dueDate:"12/5/15",
+      assignedTo:"joe",
+      location:"NSS",
+      urgency:"low",
+      dependencies:["wifi", "tissues", "vodka"]
+
+    },
+    {
+      id: 2,
+      task: "take a nap",
+      isCompleted:true,
+      dueDate:"12/5/16",
+      assignedTo:"zoe",
+      location:"Zoe's house",
+      urgency:"high",
+      dependencies:["hammock", "headphones"]
+
+    }
+  ]
+
+
+
+  $scope.newItem = function(){
+    console.log("You clicked new items");
+    $scope.showListView = false;
+  };
+
+  $scope.allItem=function(){
+    console.log("you clicked all items");
+    $scope.showListView = true;
+  };
+
 
 });
